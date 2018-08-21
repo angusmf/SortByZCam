5509fedbfea907d395b7bb1e1422a3836c37a491
angus 'my name isn't angus' freeman <angusmf@gmail.com>
2018-08-20 20:08:52 -0400

    Add two MonoBehaviour components: SortByZCam: attached to the a camera. It invokes delegates when changing from facing forward or backward so the sprites can adjust their sorting order to match. RenderOrderFixer: attached to each sprite. When the camera changes backward/foreward facing or when the sprite moves, the render order is re-set

---
+++ RenderOrderFixer.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+
+public class RenderOrderFixer : MonoBehaviour
+{
+    SpriteRenderer _spriteRenderer;
+    Camera _cam;
+
+    public bool ignorePresetOrder = true;
+    public bool startFacingForward = true;
+
+    bool camFacingForward;
+    int _sortOrder;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _cam = SortByZCam.mainCam;
+
+        SortByZCam.OnFaceForward += FaceForward;
+        SortByZCam.OnFaceBackward += FaceBackward;
+
+        if (ignorePresetOrder)
+            if (startFacingForward) SortByZ(true);
+            else SortByZ(false);
+
+    }
+
+    private void Update()
+    {
+        SortByZ(camFacingForward);
+    }
+
+    private void FaceForward()
+    {
+        camFacingForward = true;
+    }
+
+    private void FaceBackward()
+    {
+        camFacingForward = false;
+    }
+
+    private void SortByZ(bool faceForward)
+    {   
+        int newOrder = (int)(transform.position.z * SortByZCam.worldScale);
+
+        if (newOrder == _sortOrder) return;
+        else _sortOrder = newOrder;
+
+        if (faceForward) _spriteRenderer.sortingOrder = -_sortOrder;
+        else _spriteRenderer.sortingOrder = _sortOrder;
+
+    }
+
+}
+
---
+++ SortByZCam.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+
+
+public class SortByZCam : MonoBehaviour
+{
+    public static Camera mainCam;
+    public static int worldScale = 1000;
+
+
+   public delegate void FaceForward();
+    public delegate void FaceBackward();
+
+    public static FaceForward OnFaceForward;
+
+    public static FaceBackward OnFaceBackward;
+
+    bool facingForward;
+
+    private void Start()
+    {
+        if (mainCam == null)
+            mainCam = GetComponent<Camera>();
+        else
+            Destroy(this);
+    }
+
+    private void Update()
+    {
+
+        float angle = Vector3.Angle(transform.forward, Vector3.forward);
+        if (facingForward)
+            if (angle > 90)
+            {
+                facingForward = false;
+                OnFaceBackward();
+            }
+            else return;
+        else
+            if (angle < 90)
+        {
+            facingForward = true;
+            OnFaceForward();
+        }
+    }
+}
+
